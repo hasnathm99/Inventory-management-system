45e4d62b8c7e41a209a3cb08439865c9f6d37cb9
sudipta <sudipta.just@gmail.com>
2018-12-08 16:11:16 +0600

    checkpoint-5

--- inc.process/add_purchase_process.php
+++ inc.process/add_purchase_process.php
@@ -8,8 +8,8 @@ if(isset($_POST["product_name"]))
  for($count = 0; $count < count($_POST["product_name"]); $count++)
  {  
   $query = "INSERT INTO purchase 
-  (company_name, dc_no, dc_date, order_no, order_date, product_name, mt, ream, unit_price, d_price, td_price, vat, t_vat, total) 
-  VALUES (:company_name, :dc_no, :dc_date, :order_no, :order_date, :product_name, :mt, :ream, :unit_price, :d_price, :td_price, :vat, :t_vat, :total)
+  (company_name, dc_no, dc_date, order_no, order_date, product_name, ream, unit_price, total) 
+  VALUES (:company_name, :dc_no, :dc_date, :order_no, :order_date, :product_name, :ream, :unit_price, :total)
   ";
   $statement = $connect->prepare($query);
   $statement->execute(
@@ -21,13 +21,8 @@ if(isset($_POST["product_name"]))
     ':order_no'  => $_POST["order_no"],
     ':order_date'  => $_POST["order_date"], 
     ':product_name'  => $_POST["product_name"][$count],
-    ':mt'  => $_POST["mt"][$count],
     ':ream' => $_POST["ream"][$count], 
     ':unit_price' => $_POST["unit_price"][$count], 
-    ':d_price' => $_POST["d_price"][$count], 
-    ':td_price' => $_POST["td_price"][$count], 
-    ':vat' => $_POST["vat"][$count], 
-    ':t_vat' => $_POST["t_vat"][$count], 
     ':total' => $_POST["total"][$count], 
    )
   );
